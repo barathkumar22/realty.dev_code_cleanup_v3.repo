704d1716ab095ed0a4b78708da345a7d48b1eb94
barathkumar22 <94595263+barathkumar22@users.noreply.github.com>
2021-12-16 11:58:14 +0530

    dev-code-cleanup-v3.subrepo

--- src/main/java/com/example/realty/requirementrepository/Subrequirementrepo.java
+++ src/main/java/com/example/realty/requirementrepository/Subrequirementrepo.java
@@ -10,6 +10,7 @@ import java.util.Collection;
 import java.util.List;
 
 @Repository
+
 public interface Subrequirementrepo extends JpaRepository<Subrequirementrepo,Long> {
 
     @Query("UPDATE SubRequirement c SET c.SubRequirement = :SubRequirement WHERE c.id = :SubRequirementId")
